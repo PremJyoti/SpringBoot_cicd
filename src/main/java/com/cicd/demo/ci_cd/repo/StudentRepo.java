435b09c5bb04a5c94f003b90fdc071d216e2d8ab
PremJyoti <92357982+PremJyoti@users.noreply.github.com>
2024-11-27 18:51:02 +0530

    Delete src directory

--- src/main/java/com/cicd/demo/ci_cd/repo/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.cicd.demo.ci_cd.repo;
-
-import com.cicd.demo.ci_cd.enitity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface StudentRepo extends JpaRepository<Student,Integer> {
-}
